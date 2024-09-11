560938adb0cd311156eebd745c4681007c1b3fbf
tinarao <golovindanila1613@gmail.com>
2024-09-12 00:36:58 +0300

    it can decompress now
    it's colorful btw pog

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,72 @@
+use colored::Colorize;
 use std::{
-    env,
     fs::File,
     io::{self, Read, Write},
     path::{Path, PathBuf},
-    process, u64,
+    time::Instant,
+    u64,
 };
-
-use zip::{result::ZipError, write::FileOptions, CompressionMethod, ZipWriter};
-
-// format: ./compressor <target file> <output archive name>
-//         ./compressor ./src/main.rs ./src/main.zip
+use zip::{result::ZipError, write::FileOptions, CompressionMethod, ZipArchive, ZipWriter};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        println!("Format: cargo run <target_file> <output_archive_name>");
-        process::exit(0);
+    let mut command = String::new();
+    let stdin = io::stdin();
+    println!(
+        "Commands:\n\t{} -> compress\n\t{} -> decompress",
+        "\"c\"".blue(),
+        "\"d\"".red()
+    );
+
+    command.clear();
+    let _ = stdin.read_line(&mut command);
+
+    if command.trim() == String::from("c") {
+        let mut target_file = String::new();
+        let mut output_name = String::new();
+
+        println!("{}", "which file do you wanna compress?".blue());
+        let _ = stdin.read_line(&mut target_file);
+
+        println!("{}", "how do you wanna name the archive?".blue());
+        let _ = stdin.read_line(&mut output_name);
+
+        let target_file = String::from(target_file.trim());
+        let output_name = String::from(output_name.trim());
+
+        let _ = compress(target_file, output_name);
+
+        return;
+    } else if command.trim() == String::from("d") {
+        let mut target_file = String::new();
+        let mut output_name = String::new();
+
+        println!("{}", "which file do you wanna decompress?".red());
+        let _ = stdin.read_line(&mut target_file);
+        if !target_file.trim().ends_with(".zip") {
+            println!("{}", "selected file is not .zip archive".on_red());
+            return;
+        }
+
+        println!("{}", "where to extract?".red());
+        let _ = stdin.read_line(&mut output_name);
+
+        let target_file = String::from(target_file.trim());
+        let output_name = String::from(output_name.trim());
+
+        let _ = unzip(target_file, output_name);
+        return;
     }
 
-    let target_path = String::from(&args[1]);
-    let output_name = String::from(&args[2]);
-
-    let _ = compress(target_path, output_name);
+    panic!("unknown command.")
 }
 
 fn compress(target_path: String, output_name: String) -> Result<(), ZipError> {
     let exists = Path::new(&target_path);
-    if !exists.is_file() {
+    if !exists.exists() {
         panic!("you fool")
     }
 
+    let started = Instant::now();
     let archive_name = format!("{output_name}.zip");
 
     let zip_file_path = Path::new(archive_name.as_str());
@@ -50,6 +87,38 @@ fn compress(target_path: String, output_name: String) -> Result<(), ZipError> {
 
     zip.write_all(&buf)?;
     zip.finish()?;
+
+    println!("Finished in {} sec.", started.elapsed().as_secs());
+
+    Ok(())
+}
+
+fn unzip(archive_path: String, target_dir: String) -> Result<(), ZipError> {
+    let zip_file_path = Path::new(&archive_path);
+    let zip_file = File::open(zip_file_path)?;
+
+    let started = Instant::now();
+    let mut archive = ZipArchive::new(zip_file)?;
+    let extract_to = Path::new(target_dir.as_str());
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let file_name = file.name().to_owned();
+
+        let target_path = extract_to.join(file_name);
+        if let Some(parent_dir) = target_path.parent() {
+            std::fs::create_dir_all(parent_dir)?;
+        }
+
+        let mut output_file = File::create(&target_path)?;
+        io::copy(&mut file, &mut output_file)?;
+    }
+
+    println!(
+        "Finished in {}\nExtracted to {}",
+        started.elapsed().as_secs(),
+        extract_to.as_os_str().to_str().unwrap()
+    );
 
     Ok(())
 }
